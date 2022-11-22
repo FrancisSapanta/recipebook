74e5d96ddbd80e54fcd553d5ed966a8b565fb48c
FrancisSapanta <sapantafrancis@gmail.com>
2022-11-21 23:13:51 -0800

    confirmation on delete

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,7 +1,8 @@
 import React from "react";
-import {Card, CardActions, CardContent, CardMedia, Button, Typography} from '@material-ui/core';
+import {Card, CardActions, CardContent, CardMedia, Button, Typography, Tooltip, Dialog, DialogActions, DialogTitle} from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete'
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
+import VisibilityIcon from '@material-ui/icons/Visibility';
 import moment from 'moment';
 import useStyles from "./styles.js";
 import { useDispatch } from 'react-redux';
@@ -12,39 +13,61 @@ import { deletePost } from "../../../actions/posts";
 const Post = ( {post, setCurrentId} ) => {
     const classes = useStyles();
     const dispatch = useDispatch();
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+      };
+    
+      const handleClose = () => {
+        setOpen(false);
+      };
 
     return (
        <Card className={classes.card}>
             <CardMedia className={classes.media} image={post.selectedFile} title={post.title}/>
             <div className={classes.overlay}>
-                <Typography variant="h6">{post.creator}</Typography>
+                <Typography variant="h5">{post.title}</Typography>
                 <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
+                <Typography variant="body2" gutterBottom>{post.creator}</Typography>
             </div>
 
             {/* edit button */}
             <div className={classes.overlay2}>
-                <Button style={{color: 'white'}} size="small" onClick={() => setCurrentId(post._id)}>
-                    <MoreHorizIcon fontSize="medium" />
-                </Button>
+                <Tooltip title="Edit" arrow>
+                    <Button style={{color: 'white'}} size="small" onClick={() => setCurrentId(post._id)}>
+                        <MoreHorizIcon fontSize="medium" />
+                    </Button>
+                </Tooltip>
             </div>
 
 
             <div className={classes.details}>
-                <Typography variant="body2" color="textSecondary">{post.tags.map((tag) => `#${tag} `)}</Typography>
+                <Typography variant="body2" color="textSecondary">{post.tags.map((tag) => `${tag} `)}</Typography>
 
             </div>
             <CardContent>
-            <Typography className={classes.title} variant="h5" gutterBottom>{post.title}</Typography>
+            
             </CardContent>
-            <CardActions className={classes.cardActions}>
-                <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
+            <CardActions className={classes.cardActions}> 
+                <Button size="small" color="primary" onClick={() => handleClickOpen()}>
                     <DeleteIcon fontSize="small" />
                     Delete
                 </Button>
+                <Dialog open={open} onClose={handleClose} aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
+                    <DialogTitle id="alert-dialog-title">
+                        {"Are you sure you want to delete the Recipe?"}
+                    </DialogTitle>
+                    <DialogActions> 
+                        <Button onClick={handleClose}>Cancel</Button>
+                        <Button onClick={() => dispatch(deletePost(post._id))}>Delete</Button>
+                    </DialogActions>
+                </Dialog>
+                <Button color = 'primary' size="small" onClick={() => ({})}>
+                    <VisibilityIcon fontSize="medium" />
+                    View
+                </Button>
             </CardActions>
-
-            
-
        </Card>
 
     );
